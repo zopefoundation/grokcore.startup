7036854d676a6d0e1ecece048558704c3367d180
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2010-04-14 13:44:23 +0000

    partially revert 110868, re-instating the zdaemon ctl

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=['setuptools',
+                      'zdaemon',
                       'zope.component',
                       'zope.security',
                       'zope.publisher',
