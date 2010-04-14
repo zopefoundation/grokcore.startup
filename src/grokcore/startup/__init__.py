22d068f7dd54e0e288e1beebf1212afc48dd4c55
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2010-04-14 13:42:42 +0000

    Revert the removal of the zdaemon controller code to retain backwards compatibility with the ctl scripts generated by the current version of grokproject.

--- src/grokcore/startup/__init__.py
+++ src/grokcore/startup/__init__.py
@@ -14,5 +14,5 @@
 # Make this a package.
 from grokcore.startup.startup import (application_factory,
                                       debug_application_factory,
-                                      interactive_debug_prompt)
-
+                                      interactive_debug_prompt,
+                                      zdaemon_controller)
