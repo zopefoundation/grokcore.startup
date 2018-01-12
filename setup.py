2e2baf471848a2d2a39b77bbb97923798b9fd27a
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-12 14:28:24 +0100

    Back to development: 3.0.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ debug_requires = [
 
 setup(
     name='grokcore.startup',
-    version='3.0.1',
+    version='3.0.2.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
