8038ff5b446e49aa305e51b158c45ea871d047f9
Michael Howitz <icemac@gmx.net>
2025-06-18 08:38:17 +0200

    Preparing release 5.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changes
 *******
 
-5.0 (unreleased)
+5.0 (2025-06-18)
 ================
 
 - Replace ``pkg_resources`` namespace with PEP 420 native namespace.
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ debug_requires = [
 
 setup(
     name='grokcore.startup',
-    version='5.0.dev0',
+    version='5.0',
     author='Grok Team',
     author_email='zope-dev@zope.dev',
     url='https://github.com/zopefoundation/grokcore.startup',
